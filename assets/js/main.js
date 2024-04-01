201f341f42a9d854951864c3b605267cd0efebf3
Andres-Gerena-ITC <afgerenac@itc.edu.co>
2024-03-31 21:05:11 -0500

    Ajustes js
    Integración y ajustes puntuales en javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,35 +25,67 @@ else if (hora >= 0) {
 /*DOM Evento al dar clic en botón URL */
 
 function url() {
-    const urlActual = window.location.href;
+    let urlActual = window.location.href;
     alert(`Esta es la URL: ${urlActual}`);
 }
 
 
 
 
-/*Simulación de envio formulario*/
+// Agregar evento submit al formulario
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-function envioFormulario() {
+    alert("¡El envío del formulario ha sido exitoso!");
 
-    // Alerta de envio
-    alert("!SE HA ENVIADO EL FORMULARIO A ANDRÉS¡");
-
-    // Limpia formulario
-    const nombre = document.getElementById("nombre");
-    const email = document.getElementById("email");
+    // Limpiar el texto del formulario
+    let nombre = document.getElementById("name");
+    let email = document.getElementById("email");
+    let location = document.getElementById("location");
     nombre.value = "";
     email.value = "";
-}
+    location.value = "";
+});
 
 
 
-// Generando un while sencillo
+
+// Generando un while sencillo, este suma las fechas desde 2020
 
 let contador = 2020;
 
-while(contador <= 2024){
+while (contador <= 2024) {
     const elementoBloque = document.getElementById("experiencia")
-    elementoBloque.innerHTML += contador +" ";
+    elementoBloque.innerHTML += contador + " ";
     contador++;
 }
+
+
+// Indicador fecha actual
+
+let elementoFecha = document.getElementById("fecha");
+
+function actualizarFecha() {
+  let fechaActual = new Date();
+  let opciones = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  elementoFecha.textContent = fechaActual.toLocaleDateString("es-ES", opciones);
+}
+
+setInterval(actualizarFecha, 1);
+
+
+//Suma de 0 a n
+
+function sumar() {
+    const numero = parseInt(document.getElementById("numero").value);
+    let suma = 0;
+    for (let i = 0; i <= numero; i++) {
+      suma += i;
+    }
+    document.getElementById("resultado").textContent = `La suma de 0 hasta ${numero} es: ${suma}`;
+  }
